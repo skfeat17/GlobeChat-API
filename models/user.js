0b5b54c2f2ede4a788eca66892569a789c3c92dc
Sunny Kumar <skfeat177@gmail.com>
2025-09-15 21:48:40 +0530

    chore: add a newline for improved code readability in user model

--- models/user.js
+++ models/user.js
@@ -74,6 +74,7 @@ userSchema.pre("save", async function (next) {
   next();
 });
 
+
 // Check password
 userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
