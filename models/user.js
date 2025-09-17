5d57cd506cb90583424ff3aa2bde78bc620e2095
Sunny Kumar <skfeat177@gmail.com>
2025-09-17 15:38:12 +0530

    Fix formatting in user schema by correcting array syntax for blockedUsers

--- models/user.js
+++ models/user.js
@@ -60,7 +60,7 @@ const userSchema = new mongoose.Schema(
         type: mongoose.Schema.Types.ObjectId,
         ref: "User"
       }
-    ],
+    ]
     ,friends: [
       {
         type: mongoose.Schema.Types.ObjectId,
