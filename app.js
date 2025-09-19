1ae05cde3da83c1024d24e35dd35a67a26a94951
Sunny Kumar <skfeat177@gmail.com>
2025-09-19 20:41:56 +0530

    Update CORS configuration to allow all origins

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
