81e549906b4fbf696ca7f80dd8d6d1539d01cef1
Sunny Kumar <122142837+skfeat17@users.noreply.github.com>
2026-02-27 12:08:17 +0530

    Add test route to log client IP address
    Added a test route to log and return the client's IP address.

--- app.js
+++ app.js
@@ -11,7 +11,10 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
-
+app.get("/test", (req, res) => {
+  console.log(req.ip);
+  res.send(req.ip);
+});
 //routes import
 import userRouter from './routes/user.js'
 import messageRouter from './routes/message.js'
@@ -24,3 +27,4 @@ app.use(errorHandler);
 
 
 export { app }
+
